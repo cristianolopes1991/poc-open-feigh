90f8f043ba20f7bae5d5ed8ada039e06669f7522
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2023-08-26 23:07:41 -0300

    :tada: commit inicial

---
+++ src/main/java/pocopenfeigh/PocOpenFeighApplication.java
@@ -0,0 +1,13 @@
+package pocopenfeigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOpenFeighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOpenFeighApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocopenfeigh/PocOpenFeighApplicationTests.java
@@ -0,0 +1,13 @@
+package pocopenfeigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocOpenFeighApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
